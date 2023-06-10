faeaa7dd8f0f2f8ba1358a44f8073d2155f1ab56
yeonghoon.lee <djaakclsrn12@naver.com>
2023-06-10 15:28:31 +0900

    NEW: apect적용 순서 변경 버전

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -3,14 +3,21 @@ package hello.aop;
 
 import hello.aop.order.OrderRepository;
 import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV5;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
+//@Import(AspectV1.class)
+//@Import(AspectV2.class)
+//@Import(AspectV3.class)
+//@Import(AspectV4Pointcut.class)
+@Import({AspectV5.LogAspect.class, AspectV5.TransactionAspect.class})
 @SpringBootTest
 public class AopTest {
 
